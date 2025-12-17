e8ba140f83857af090810e514ba5a6ec762f24fe
hyperoot <rajesh@hyperoot.dev>
2025-12-17 03:21:23 +0000

    chore: clean up setup script for project initialization

--- scripts/setup_project.sh
+++ scripts/setup_project.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 if ! command -v cz >/dev/null; then
-    git config --global push.autoSetupRemote true
-    git config --global --add safe.directory '*'
-    pip install --user pipx
-    pipx install commitizen
-    pre-commit install
-    pre-commit install --hook-type commit-msg
+  pip install --user pipx
+  pipx install commitizen
+fi
+
+if [ ! -f .git/hooks/pre-commit ]; then
+  pre-commit install
+fi
+
+if [ ! -f .git/hooks/commit-msg ]; then
+  pre-commit install --hook-type commit-msg
 fi
